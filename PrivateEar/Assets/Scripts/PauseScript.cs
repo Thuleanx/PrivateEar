08c931944e20aff6625a2a520ddfe2542f3e9e53
JKHackathon <penguinkim21@gmail.com>
2022-10-15 15:58:27 -0400

    Pause Progress

--- PrivateEar/Assets/Scripts/PauseScript.cs
+++ PrivateEar/Assets/Scripts/PauseScript.cs
@@ -2,32 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class pauseScript : MonoBehaviour
+public class PauseScript : MonoBehaviour
 {
     public GameObject pauseMenu;
+    public GameObject pauseCanvas;
     float targetPos = 0f;
 
+    static int PAUSE = 0, ENDPAUSE = 1, NEUTRAL = 2;
+    int pauseState = 2;
 
     // Start is called before the first frame update
     void Start()
     {
         pauseMenu.GetComponent<Transform>().position.Set(0, -10, 0);
+        pauseCanvas.SetActive(false);
         //PauseAnimation();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (pauseState==PAUSE)
+        {
+            PauseAnimation();
+        }
+        if (pauseState == ENDPAUSE)
+        {
+            ExitPauseAnimation();
+        }
     }
+
+    public void TriggerPauseScreen() => pauseState = PAUSE;
+
+    public void TriggerExitPause() => pauseState = ENDPAUSE;
 
     void PauseAnimation()
     {
-        //Not the most efficient way of doing this, maybe just make an animation
-        while (pauseMenu.GetComponent<Transform>().position.y != targetPos)
+        //This is if we want a pause animation from below into position
+        //Additionally, do we want to blur the background?
+        /*if (false)//pauseMenu.GetComponent<Transform>().position.y != targetPos)
         {
-            //pauseMenu.GetComponent<Transform>().position.Set();
+            float y = pauseMenu.GetComponent<Transform>().position.y + 1;
+            pauseMenu.GetComponent<Transform>().position.Set(0,y,0);
         }
+        else
+        {
+            pauseCanvas.SetActive(true);
+            pauseState = NEUTRAL;
+        }*/
+        pauseCanvas.SetActive(true);
+        pauseState = NEUTRAL;
+    }
+
+    void ExitPauseAnimation()
+    {
+        pauseCanvas.SetActive(false);
+
     }
 
 }
--- PrivateEar/Assets/Scripts/SceneChanger.cs
+++ PrivateEar/Assets/Scripts/SceneChanger.cs
@@ -11,17 +11,19 @@ namespace PrivateEar {
 		public GameObject blackScreen;
 		public Canvas fadeCanvas;
 		public SceneReference sceneReference;
+		public bool isPause;
 
 		static int FADEIN = 0, FADEOUT = 1, NEUTRAL = 2;
 		int fadeState;
 
 		public void Start() {
-			//Set fade screen to top layer
-			//NOTE: THIS REQUIRES SETTING UP SPECIFIC LAYERS TO DISTINGUISH FADE
-			//blackScreen.layer = LayerMask.NameToLayer("IgnoreRaycast");
-			fadeState = FADEIN;
+			if (isPause) { fadeState = NEUTRAL; }
+			else
+			{
+				fadeState = FADEIN;
+				fadeCanvas.GetComponent<CanvasGroup>().alpha = 1;
+			}
 			blackScreen.GetComponent<Image>().color = new Color(0, 0, 0, 1);
-			fadeCanvas.GetComponent<CanvasGroup>().alpha = 1;
 		}
 
 		public void Update() {

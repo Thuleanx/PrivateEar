9188b4c429db851fa78f9555c5e58eb97514268d
An-Nguy <59397202+Thuleanx@users.noreply.github.com>
2022-10-15 14:56:27 -0500

    outline on hover

--- PrivateEar/Assets/Scripts/CObject.cs
+++ PrivateEar/Assets/Scripts/CObject.cs
@@ -7,10 +7,21 @@ namespace PrivateEar {
 		public static CObject HoveredObject = null;
 
 		[SerializeField, ReadOnly] Marker _matchedMarker;
+		public SpriteRenderer Sprite { get; private set; }
+
+		[Header("Sprite")]
+		[SerializeField] string spriteOutlineFieldName = "_Outline";
+		[SerializeField] float outlineThicknessOnHover;
 
 		public Marker MatchedMarker {
 			get => _matchedMarker;
 			set { _matchedMarker = value; }
+		}
+
+		private void Awake() {
+			Sprite = GetComponentInChildren<SpriteRenderer>();
+			Sprite.material = new Material(Sprite.material);
+			Sprite.material.SetFloat(spriteOutlineFieldName, 0f);
 		}
 
 		public void Start() {
@@ -19,9 +30,11 @@ namespace PrivateEar {
 
 		public void OnHoverEnter() {
 			HoveredObject = this;
+			Sprite.material.SetFloat(spriteOutlineFieldName, outlineThicknessOnHover);
 		}
 
 		public void OnHoverExit() {
+			Sprite.material.SetFloat(spriteOutlineFieldName, 0f);
 			if (HoveredObject == this) HoveredObject = null;
 		}
 

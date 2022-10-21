487d01cd6d70e51bf802af24b143d2fd1ea9f93c
An-Nguy <59397202+Thuleanx@users.noreply.github.com>
2022-10-21 15:59:32 -0500

    loading scene and some music

---
+++ PrivateEar/Assets/Scripts/Loading.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using System;
+
+using PrivateEar.Utils;
+
+namespace PrivateEar {
+	public class Loading : MonoBehaviour {
+		public SceneReference Next;
+		void Update() {
+			try {
+				if (FMODUnity.RuntimeManager.HaveMasterBanksLoaded) {
+					Debug.Log("Master Bank Loaded");
+					Next.LoadScene();
+				} else {
+					Debug.Log("Master Bank Not Yet Loaded " + FMODUnity.RuntimeManager.AnySampleDataLoading());
+				}
+			} catch (Exception err) {
+				Debug.Log(err);
+			}
+
+		}
+	}
+}

eaa8a54e4c4d06c33d61d6e6cfb24316ba9c8939
Thuleanx <101783970+An-Nguy@users.noreply.github.com>
2022-11-18 12:46:41 -0600

    confirmation message

--- PrivateEar/Assets/Scripts/UI/SubmitButtonHandler.cs
+++ PrivateEar/Assets/Scripts/UI/SubmitButtonHandler.cs
@@ -5,19 +5,32 @@ namespace PrivateEar {
 		public SubmitButton Button { get; private set; }
 		bool _allMatchedLastFrame;
 		bool _firstFrame;
+		bool _submitAppear;
 
 		private void Awake() {
 			Button = GetComponentInChildren<SubmitButton>();
 		}
 
-		private void OnEnable() { _firstFrame = true;  }
+		private void OnEnable() { 
+			_firstFrame = true;  
+			_submitAppear = false;
+		}
+
+		void Start() {
+			foreach (var marker in FindObjectsOfType<Marker>())
+				marker.OnCObjectAssigned.AddListener(()=>{
+					if (GameMaster.Instance.IsAllMatched)
+						_submitAppear = true;
+				});
+		}
 
 		private void Update() {
 			if (Button) {
 				bool allMatched = GameMaster.Instance.IsAllMatched;
-				if (_firstFrame || _allMatchedLastFrame != allMatched) {
+				if (_firstFrame || _allMatchedLastFrame != allMatched || _submitAppear) {
 					if (allMatched && !Button.Active) Button.Appear();
 					else if (!allMatched && Button.Active) Button.Disappear();
+					_submitAppear = false;
 				}
 				_firstFrame = false;
 				_allMatchedLastFrame = allMatched;

6fdeb5dddcd03a7e7bf5dac4fcd7e51c11914a03
lena <lena_he@brown.edu>
2022-11-19 14:41:26 -0500

    title screen buttons + new credits page

---
+++ PrivateEar/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoader2 : MonoBehaviour
+{
+   public void QuitGame()
+   {
+        Application.Quit();
+        Debug.Log("Quit!");
+   }
+}
